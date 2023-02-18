fe75157fe4af7ff6d38adfb384d7abbe2eecdf29
James McNair <booksaw224@gmail.com>
2023-02-18 16:53:06 +0000

    Test QueryStringUtils

---
+++ front-end/src/utils/QueryStringUtils.test.ts
@@ -0,0 +1,28 @@
+import { assert } from "console";
+import { getQueryString, getURL } from "./QueryStringUtils";
+
+test("Test get current query string", () => {
+    const windowSpy: any = jest.spyOn(window, "window","get");
+
+    windowSpy.mockImplementation(() => ({
+        location: {
+            search: "test=1&test2=2"
+        }
+    }))
+
+    const queryString = getQueryString();
+    expect(queryString).toEqual({"test": "1", "test2": "2"});
+}); 
+
+test("Build URL with params", () => {
+
+    const url = getURL("test", {query: "test1"});
+
+    expect(url).toEqual("test?query=test1")
+});
+
+test("Build URL without params", () => {
+    const url = getURL("test", {});
+
+    expect(url).toEqual("test");
+});
